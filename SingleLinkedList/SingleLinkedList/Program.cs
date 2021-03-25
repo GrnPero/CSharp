5ddfe993b7998158dc8c17ee1ad2fd1f3719b511
Rodrigo Alvarez <44035269+GrnPero@users.noreply.github.com>
2021-03-24 19:54:09 -0700

    Successfully implemented a Single Linked List

---
+++ SingleLinkedList/SingleLinkedList/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingleLinkedList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SingleLinkedList LinkedList = new SingleLinkedList();
+
+            LinkedList.Add(1);
+            LinkedList.Add(10);
+            LinkedList.Add(4);
+            LinkedList.Add(22);
+
+            LinkedList.Print();
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ SingleLinkedList/SingleLinkedList/SingleLinkedList.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingleLinkedList { 
+    class SingleLinkedList { 
+        public class Node {
+            public int value;
+            public Node right;
+
+
+            public Node(int value) {
+                this.value = value;
+                right = null;
+            }
+        }
+
+        Node root;
+        
+        public SingleLinkedList() {
+            root = null;
+        }
+
+        public void Add(int value) {
+            root = AddRecursion(root, value);
+        }
+
+        public Node AddRecursion(Node root, int value) {
+            // Link List is empty
+            if (root == null) {
+                root = new Node(value);
+                return root;
+            }
+
+            // Link list already has an existing value in the Node.right so we use recursion to find an empty Node
+            if (root != null) {
+                root.right = AddRecursion(root.right, value);
+            }
+            // We return the new object that was created from this recursion
+            return root;
+        }
+
+        public void Print() {
+            RecursionPrint(root);
+        }
+
+        public void RecursionPrint(Node root) {
+            if (root != null) {
+                Console.WriteLine(root.value);
+                RecursionPrint(root.right);
+            }
+        }
+
+        
+
+
+
+    }
+}
